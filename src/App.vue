5d454d9870d284efa5c93c59d7a3430766988405
Ethiel ADIASSA <ethiel97@gmail.com>
2018-11-13 01:29:10 +0000

    First commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <HelloWorld msg="Simple data visualisation with Vue.js and Chart.js"/>
+    <visualisation></visualisation>
+
+  </div>
+</template>
+
+<script>
+/* eslint-disable */
+
+import Visualisation from './components/Visualisation.vue';
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Visualisation,
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
